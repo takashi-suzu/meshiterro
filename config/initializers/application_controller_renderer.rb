9808a8d82103439b590dc1445f4edb66971b98bd
takashi-suzu <t.suzuki33333@gmail.com>
2020-05-11 17:55:23 +0900

    [5-3]finish

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'f391f7074dba5194ed232009581f6bfe593c60afeac8f13bfd91b5431dc6215265e469daa741b7bcdd29a068d9a039bfbc14d14a8c9ebbdaf9bbc69f3a294aed'
